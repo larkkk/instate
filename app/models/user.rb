74a9c4713bce6579158ab53e70c67d52321cbc91
lark <larkl4rk@gmail.com>
2016-11-12 15:10:22 +0900

    add like model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :identify, uniqueness: true
-  has_many :posts
+  has_many :posts, :likes
+
+  has_many :liked_posts, through: :likes, source: :post
 end
