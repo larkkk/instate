65cfc416018692ab0ac76f002181b25e132696f5
lark <larkl4rk@gmail.com>
2016-11-12 12:59:47 +0900

    유저가 로그인상태가 아닐 경우 회원가입페이지로 이동

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
   def index
+    # 유저가 로그인상태가 아닐 경우 회원가입페이지로 이동
+    unless user_signed_in?
+      redirect_to new_user_registration_path
+    end
   end
 end
