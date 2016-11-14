1f6f0eb66529a581e8f243bfdca7f32e82ee8003
lark <larkl4rk@gmail.com>
2016-11-14 14:02:39 +0900

    이미지 포스팅시 이미지 크기는 최대 600 * 750으로 설정

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -4,7 +4,7 @@ class PostImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -27,6 +27,9 @@ class PostImageUploader < CarrierWave::Uploader::Base
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #
+
+  process :resize_to_limit => [600, 750]
+
   # def scale(width, height)
   #   # do something
   # end
