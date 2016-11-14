d76b3a79e8efe8afd259f88bdd83b1463850431f
lark <larkl4rk@gmail.com>
2016-11-14 18:44:06 +0900

    모든 유저 출력 페이지 완성

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,6 +1,6 @@
 class ExploreController < ApplicationController
   layout 'post'
-  
+
   def index
     @user_list = User.all
   end
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,6 +2,6 @@ class ProfileController < ApplicationController
   layout 'post'
 
   def index
-    @find_user = User.find_by(identify: params[:identify])
+    @find_user = User.find_by(identify: params[:id])
   end
 end
