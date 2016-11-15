1c9423b2f995643610d5c31c7c4c2e1df61ed788
lark <larkl4rk@gmail.com>
2016-11-15 13:28:59 +0900

    프로필페이지 글 최신순

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,5 +3,6 @@ class ProfileController < ApplicationController
 
   def index
     @find_user = User.find_by(identify: params[:id])
+    @posts = @find_user.posts.order('created_at desc')
   end
 end
